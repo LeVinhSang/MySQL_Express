7eab6826fafea76d91af1cdddb3c2b9626fcfb03
LeVinhSang <sanglv@sphinx-software.com>
2018-02-22 14:11:12 +0700

    Add Begin

---
+++ app.js
@@ -0,0 +1,56 @@
+let express = require('express');
+let mysql = require('mysql');
+let app = express();
+let connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '1',
+    database : 'CRUDBook'
+});
+
+/*connection.query('CREATE DATABASE CRUDBook', (err) => {
+   if(err) throw err;
+   connection.query('USE CRUDBook', (err) => {
+       if(err) throw err;
+       connection.query('create table Book ('
+           + "id int not null primary key auto_increment,"
+           + "title nvarchar(40) not null ,"
+           + "author nvarchar(40) not null,"
+           + "publister text,"
+           + "price float"
+           + ')', (err) => {
+           if(err) throw err;
+       });
+   })
+});*/
+
+app.get('/user/:title', (req,res) => {
+   connection.query("select * from Book where title =?", req.param('title'), (err, result) => {
+       if(err) throw err;
+       res.send(result);
+       //res.send(result[1].author);
+   });
+});
+app.post('/', (req, res) => {
+    connection.query('insert into Book values (NULL, "Dragon Ball Z", "Akira Toriyama", "Japan", NULL)',
+        (err) => {
+        if(err) throw err;
+        res.send('User added to database');
+    });
+});
+
+app.put('/user/update', (req, res) => {
+   connection.query('update Book set price ="1000" where id="3"', (err) => {
+       if(err) throw err;
+       res.send("Success!");
+   });
+});
+app.delete('/user/delete', (req, res) => {
+   connection.query('delete from Book where id="3"', (err) => {
+       if(err) throw err;
+       res.send('Success!');
+   });
+});
+app.listen(3000, () => {
+    console.log("Sever running");
+});
