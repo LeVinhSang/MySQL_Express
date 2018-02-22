342b5baa29d4409186d9a7bda9ca90fae90da1f6
LeVinhSang <sanglv@sphinx-software.com>
2018-02-22 15:10:32 +0700

    Add update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let mysql = require('mysql');
 let app = express();
+let bodyParse = require('body-parser');
 let connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
@@ -16,27 +17,45 @@ let connection = mysql.createConnection({
            + "id int not null primary key auto_increment,"
            + "title nvarchar(40) not null ,"
            + "author nvarchar(40) not null,"
-           + "publister text,"
+           + "pubLister text,"
            + "price float"
            + ')', (err) => {
            if(err) throw err;
        });
    })
 });*/
+app.use(bodyParse.urlencoded({extended:true}));
+app.use(bodyParse.json());
 
-app.get('/user/:title', (req,res) => {
+app.get('/user', (req,res) => {
+    connection.query("select * from Book", (err, result) => {
+        if(err) throw err;
+        res.send(result);
+        //res.send(result[1].author);
+    });
+});
+
+/*app.get('/user/:title', (req,res) => {
    connection.query("select * from Book where title =?", req.param('title'), (err, result) => {
        if(err) throw err;
        res.send(result);
        //res.send(result[1].author);
    });
-});
-app.post('/', (req, res) => {
+});*/
+/*app.post('/', (req, res) => {
     connection.query('insert into Book values (NULL, "Dragon Ball Z", "Akira Toriyama", "Japan", NULL)',
         (err) => {
         if(err) throw err;
         res.send('User added to database');
     });
+});*/
+
+app.post('/', (req, res) => {
+    connection.query('insert into Book set ?', req.body,
+        (err) => {
+            if(err) throw err;
+            res.send('User added to database');
+        });
 });
 
 app.put('/user/update', (req, res) => {
@@ -45,12 +64,19 @@ app.put('/user/update', (req, res) => {
        res.send("Success!");
    });
 });
-app.delete('/user/delete', (req, res) => {
+
+app.delete('/user/:id', (req, res) => {
+    connection.query('delete from Book where id=?', req.param('id'), (err) => {
+        if(err) throw err;
+        res.send('Success!');
+    });
+});
+/*app.delete('/user/delete', (req, res) => {
    connection.query('delete from Book where id="3"', (err) => {
        if(err) throw err;
        res.send('Success!');
    });
-});
+});*/
 app.listen(3000, () => {
     console.log("Sever running");
 });
